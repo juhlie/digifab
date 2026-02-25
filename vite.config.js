4aeee3ac8797b1e8388c4e98a0f76dd79230a463
Julie Lam <43072938+juhlie@users.noreply.github.com>
2026-02-25 03:36:10 -0500

    remove glitch dev config settings

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build"
+    outDir: "build",
   },
   server: {
-    host:"0.0.0.0",
-    port:3000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
-    }
-  }
+    port: 3000,
+  },
 });
